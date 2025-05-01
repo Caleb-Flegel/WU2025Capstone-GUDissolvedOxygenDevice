18c98cc9aa75120b62f6e766e75c0d9eec564ba7
Caleb <cflegel24@my.whitworth.edu>
2025-05-01 13:56:45 -0700

    Changes to Arduino program from testing

--- Arduino/1Probe2Pump/1Probe2Pump.ino
+++ Arduino/1Probe2Pump/1Probe2Pump.ino
@@ -1,10 +1,10 @@
-#include "do_surveyor.h"
+  #include "do_surveyor.h"
 // Connect the DO probe at A0
 Surveyor_DO DO = Surveyor_DO(A0);
 
 // Define Pump control pins
 const int agitationPump = 15; //AKA A1
-const int aerationPump = 16;  //AKA A0
+const int aerationPump = 16;  //AKA A2
 
 uint8_t user_bytes_received = 0;
 const uint8_t bufferlen = 32;
@@ -13,6 +13,8 @@ char user_data[bufferlen];
 void parse_cmd(char* string) {
   strupr(string);
   String cmd = String(string);
+  
+  // Serial.println(cmd.c_str());
 
   // Calibration command code from ATLAS DO library
   if(cmd.startsWith("CAL")){
@@ -29,35 +31,42 @@ void parse_cmd(char* string) {
       Serial.println("DO CALIBRATED");
     }
   }
+  
   // Pump command code 
   //  Enabling pumps
-  else if (cmd.startsWith("EnablePump")) {
+  else if (cmd.startsWith("ENABLEPUMP")) {
+    // Serial.print("Enabling Pump");
     int index = cmd.indexOf('-');
     if (index != -1) {
       String pump = cmd.substring(index+1, cmd.length());
 
       if(pump.equals("0")) {
+        Serial.println("Enabling Pump 0");
         // Enable pump 0 / agitation pump
-        digitalWrite(agitationPump, HIGH)
+        digitalWrite(agitationPump, HIGH);
       }
       else if(pump.equals("1")) {
+        Serial.println("Enabling Pump 1");
         // Enable pump 1 / aeration pump
-        digitalWrite(aerationPump, HIGH)
+        digitalWrite(aerationPump, HIGH);
       }
     }
   }
-  else if (cmd.startsWith("DisablePump")) {
+  else if (cmd.startsWith("DISABLEPUMP")) {
+    // Serial.print("Disabling Pump");
     int index = cmd.indexOf('-');
     if (index != -1) {
       String pump = cmd.substring(index+1, cmd.length());
 
       if(pump.equals("0")) {
+        Serial.println("Disabling Pump 0");
         // Disable pump 0 / agitation pump
-        digitalWrite(agitationPump, LOW)
+        digitalWrite(agitationPump, LOW);
       }
       else if(pump.equals("1")) {
+        Serial.println("Disabling Pump 1");
         // Disable pump 1 / aeration pump
-        digitalWrite(aerationPump, LOW)
+        digitalWrite(aerationPump, LOW);
       }
     }
   }
@@ -71,12 +80,14 @@ void setup() {
   Serial.begin(9600);
   delay(200);
   // Serial.println(F("Use command \"CAL\" to calibrate the circuit to 100% saturation in air\n\"CAL,CLEAR\" clears the calibration"));
-  if(DO.begin()){
-    // Serial.println("Loaded EEPROM");
+  if(!DO.begin()){
+    Serial.println("ERROR leading EEPROM");
   }
 }
 
 void loop() {
+  delay(500);
+
   if (Serial.available() > 0) {
     user_bytes_received = Serial.readBytesUntil(13, user_data, sizeof(user_data));
   }
@@ -88,5 +99,4 @@ void loop() {
   }
   
   Serial.println(DO.read_do_percentage());
-  delay(1000);
 }
